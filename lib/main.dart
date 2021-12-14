6da843bbb22414b98728d8c8991278f6abf41422
MSI Modern 14 <sakadararaihan@gmail.com>
2021-12-14 11:47:11 +0700

    kalkulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:math_expressions/math_expressions.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
 
 void main() {
-  runApp(Calculator());
+  runApp(kalkulator());
 }
 
-class Calculator extends StatelessWidget {
-  const Calculator({Key? key}) : super(key: key);
+class kalkulator extends StatelessWidget {
+  const kalkulator({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Kalkulator',
-      theme: ThemeData(primarySwatch: Colors.grey),
-      home: SimpleCalculator(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: demo(),
     );
   }
 }
 
-class SimpleCalculator extends StatefulWidget {
-  const SimpleCalculator({Key? key}) : super(key: key);
+class demo extends StatefulWidget {
+  const demo({Key? key}) : super(key: key);
 
   @override
-  _SimpleCalculatorState createState() => _SimpleCalculatorState();
+  _demo createState() => _demo();
 }
 
-class _SimpleCalculatorState extends State<SimpleCalculator> {
+class _demo extends State<demo> {
   String equation = "0";
   String result = "0";
   String expression = "";
@@ -102,88 +102,160 @@ class _SimpleCalculatorState extends State<SimpleCalculator> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Kalkulator')),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
-            child: Text(
-              equation,
-              style: TextStyle(fontSize: equationFontSize),
+    return InnerDrawer(
+
+        // key: _innerDrawerKey,
+        onTapClose: true, // default false
+        swipe: true, // default true
+        colorTransitionChild: Colors.blue, // default Color.black54
+        colorTransitionScaffold: Colors.black54, // default Color.black54
+
+        //When setting the vertical offset, be sure to use only top or bottom
+        offset: IDOffset.only(bottom: 0.00, right: 0.00, left: 0.00),
+        scale: IDOffset.horizontal(1), // set the offset in both directions
+
+        proportionalChildArea: true, // default true // default 0
+        leftAnimationType: InnerDrawerAnimation.static, // default static
+        rightAnimationType: InnerDrawerAnimation.quadratic,
+        backgroundDecoration: BoxDecoration(
+            color:
+                Colors.red[200]), // default  Theme.of(context).backgroundColor
+
+        //when a pointer that is in contact with the screen and moves to the right or left
+        onDragUpdate: (double val, InnerDrawerDirection? direction) {
+          // return values between 1 and 0
+          print(val);
+          // check if the swipe is to the right or to the left
+          print(direction == InnerDrawerDirection.start);
+        },
+        innerDrawerCallback: (a) =>
+            print(a), // return  true (open) or false (close)
+
+        leftChild: Material(
+          child: Container(
+            color: Colors.blueAccent,
+            child: ListView(
+              children: <Widget>[
+                const SizedBox(height: 20),
+                ListTile(
+                  title: Text(
+                    "Kalkulator",
+                    style: TextStyle(color: Colors.white, fontSize: 20),
+                  ),
+                )
+              ],
             ),
           ),
-          Container(
-            alignment: Alignment.centerRight,
-            padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
-            child: Text(
-              result,
-              style: TextStyle(fontSize: resultFontSize),
-            ),
-          ),
-          Expanded(child: Divider()),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+        ),
+
+        // required if rightChild is not set
+        rightChild: Container(), // required if leftChild is not set
+
+        //  A Scaffold is generally used but you are free to use other widgets
+        // Note: use "automaticallyImplyLeading: false" if you do not personalize "leading" of Bar
+
+        scaffold: Scaffold(
+          appBar: AppBar(
+              title: Text("Kalkulator"), automaticallyImplyLeading: true),
+          body: Column(
+            children: <Widget>[
               Container(
-                width: MediaQuery.of(context).size.width * .70,
-                child: Table(
-                  children: [
-                    TableRow(children: [
-                      buildButton("C", 1, Colors.grey),
-                      buildButton("⌫", 1, Colors.grey),
-                      buildButton("÷", 1, Colors.grey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("7", 1, Colors.blueGrey),
-                      buildButton("8", 1, Colors.blueGrey),
-                      buildButton("9", 1, Colors.blueGrey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("4", 1, Colors.blueGrey),
-                      buildButton("5", 1, Colors.blueGrey),
-                      buildButton("6", 1, Colors.blueGrey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("1", 1, Colors.blueGrey),
-                      buildButton("2", 1, Colors.blueGrey),
-                      buildButton("3", 1, Colors.blueGrey)
-                    ]),
-                    TableRow(children: [
-                      buildButton(".", 1, Colors.blueGrey),
-                      buildButton("0", 1, Colors.blueGrey),
-                      buildButton("00", 1, Colors.blueGrey),
-                    ]),
-                  ],
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(10, 20, 10, 0),
+                child: Text(
+                  equation,
+                  style: TextStyle(fontSize: equationFontSize),
                 ),
               ),
               Container(
-                width: MediaQuery.of(context).size.width * 0.30,
-                child: Table(
-                  children: [
-                    TableRow(children: [
-                      buildButton("×", 1, Colors.grey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("-", 1, Colors.grey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("+", 1, Colors.grey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("%", 1, Colors.grey),
-                    ]),
-                    TableRow(children: [
-                      buildButton("=", 1, Colors.orangeAccent),
-                    ]),
-                  ],
+                alignment: Alignment.centerRight,
+                padding: EdgeInsets.fromLTRB(10, 30, 10, 0),
+                child: Text(
+                  result,
+                  style: TextStyle(fontSize: resultFontSize),
                 ),
-              )
+              ),
+              Expanded(child: Divider()),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width * .70,
+                    child: Table(
+                      children: [
+                        TableRow(children: [
+                          buildButton("C", 1, Colors.grey),
+                          buildButton("⌫", 1, Colors.grey),
+                          buildButton("÷", 1, Colors.grey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("7", 1, Colors.blueGrey),
+                          buildButton("8", 1, Colors.blueGrey),
+                          buildButton("9", 1, Colors.blueGrey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("4", 1, Colors.blueGrey),
+                          buildButton("5", 1, Colors.blueGrey),
+                          buildButton("6", 1, Colors.blueGrey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("1", 1, Colors.blueGrey),
+                          buildButton("2", 1, Colors.blueGrey),
+                          buildButton("3", 1, Colors.blueGrey)
+                        ]),
+                        TableRow(children: [
+                          buildButton(".", 1, Colors.blueGrey),
+                          buildButton("0", 1, Colors.blueGrey),
+                          buildButton("00", 1, Colors.blueGrey),
+                        ]),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.30,
+                    child: Table(
+                      children: [
+                        TableRow(children: [
+                          buildButton("×", 1, Colors.grey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("-", 1, Colors.grey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("+", 1, Colors.grey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("%", 1, Colors.grey),
+                        ]),
+                        TableRow(children: [
+                          buildButton("=", 1, Colors.orangeAccent),
+                        ]),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ],
           ),
-        ],
-      ),
-    );
+          /* OR
+            CupertinoPageScaffold(                
+                navigationBar: CupertinoNavigationBar(
+                    automaticallyImplyLeading: false
+                ),
+            ), 
+            */
+        ));
   }
+}
+
+//  Current State of InnerDrawerState
+final GlobalKey<InnerDrawerState> _innerDrawerKey =
+    GlobalKey<InnerDrawerState>();
+
+void _toggle() {
+  _innerDrawerKey.currentState?.toggle(
+      // direction is optional
+      // if not set, the last direction will be used
+      //InnerDrawerDirection.start OR InnerDrawerDirection.end
+      direction: InnerDrawerDirection.end);
 }
